59227085c1fe36d2e7e5ae39d525110ff810532b
Yumo Xu <kevin.xym@outlook.com>
2019-09-04 17:01:26 +0100

    commit

--- examples/run_qas.py
+++ examples/run_qas.py
@@ -552,12 +552,14 @@ def load_and_cache_examples(args, task, tokenizer, evaluate=False):
     processor = processors[task]()
     output_mode = output_modes[task]
     # Load data features from cache or dataset file
+
     cached_features_file = os.path.join(args.data_dir, 'cached_shared_{}_{}_{}_{}'.format(
         'dev' if evaluate else 'train',
         list(filter(None, args.model_name_or_path.split('/'))).pop(),
         str(args.max_seq_length),
         # str(task))
         'qas-shared'))
+
     if os.path.exists(cached_features_file):
         logger.info("Loading features from cached file %s", cached_features_file)
         features = torch.load(cached_features_file)
