f50bf958f0f0c0840a574b5b44c8121511594ffd
Yumo Xu <kevin.xym@outlook.com>
2019-09-11 21:47:14 +0100

    commit

--- examples/run_wt.py
+++ examples/run_wt.py
@@ -439,12 +439,12 @@ def load_and_cache_examples(args, task, tokenizer, evaluate=False):
     output_mode = output_modes[task]
     # Load data features from cache or dataset file
 
-    cached_features_file = os.path.join(args.data_dir, 'cached_shared_{}_{}_{}_{}'.format(
+    cached_features_file = os.path.join(args.data_dir, 'cached_{}_{}_{}_{}'.format(
         'dev' if evaluate else 'train',
         list(filter(None, args.model_name_or_path.split('/'))).pop(),
         str(args.max_seq_length),
         # str(task))
-        'qas-shared'))
+        'wt'))
 
     if os.path.exists(cached_features_file):
         logger.info("Loading features from cached file %s", cached_features_file)
