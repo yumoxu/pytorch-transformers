99f9a41770d4568e54db028dae1870a595cf52c5
Yumo Xu <kevin.xym@outlook.com>
2019-09-12 22:34:42 +0100

    commit

--- examples/run_wt.py
+++ examples/run_wt.py
@@ -445,6 +445,7 @@ def eval_birch_model(args):
 
         eval_loss = eval_loss / nb_eval_steps
         if args.output_mode == "classification":
+            logger.info('preds size: {}')
             preds = np.argmax(preds, axis=1)
         elif args.output_mode == "regression":
             preds = np.squeeze(preds)
