abe5058e873dc0f5e021219ec3236d547dbb5a8a
Yumo Xu <kevin.xym@outlook.com>
2019-12-12 19:54:40 +0000

    commit

--- examples/doc_vp/run_doc_vp.py
+++ examples/doc_vp/run_doc_vp.py
@@ -132,13 +132,16 @@ def train(args, train_dataset, model, tokenizer):
         epoch_iterator = tqdm(train_dataloader, desc="Iteration", disable=args.local_rank not in [-1, 0])
         for step, batch in enumerate(epoch_iterator):
             model.train()
+
             batch = tuple(t.to(args.device) for t in batch)
+            logger.info('Put batch onto args.device: {}'.format(args.device))
             inputs = {'input_ids':      batch[0],
                       'attention_mask': batch[1],
                       'token_type_ids': batch[2] if args.model_type in ['bert', 'xlnet'] else None,  # XLM don't use segment_ids
                       'doc_vocab':      batch[3]}
             outputs = model(**inputs)
             loss = outputs[0]  # model outputs are always tuple in pytorch-transformers (see doc)
+            logger.info('loss: {}'.format(loss))
 
             if args.n_gpu > 1:
                 loss = loss.mean() # mean() to average on multi-gpu parallel training
